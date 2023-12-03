b34c6f01d95af0e5ccb54e6746bcea995d0adda6
mr-gino <skocz.dawid@o2.pl>
2023-12-03 22:38:57 +0100

    dodanie zadania 6 lab 1

---
+++ lab1/lab1.6/Program.cs
@@ -0,0 +1,12 @@
+﻿while (true)
+{
+    Console.Write("Podaj liczbę całkowitą (aby zakończyć, wprowadź liczbę mniejszą od zera): ");
+    int liczba = Convert.ToInt32(Console.ReadLine());
+
+    if (liczba < 0)
+    {
+        Console.WriteLine("Koniec programu.");
+        break;
+    }
+
+}
