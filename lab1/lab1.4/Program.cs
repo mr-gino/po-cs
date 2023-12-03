f0e2ac2cdb8db061b367d6e10f63cfcaad602b75
mr-gino <skocz.dawid@o2.pl>
2023-12-03 22:35:29 +0100

    dodanie zadania 4 lab 1

---
+++ lab1/lab1.4/Program.cs
@@ -0,0 +1,47 @@
+﻿double[] numbers = new double[10];
+
+Console.WriteLine("Wprowadź 10 liczb:");
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write($"Liczba {i + 1}: ");
+    numbers[i] = Convert.ToDouble(Console.ReadLine());
+}
+Console.WriteLine("\n");
+
+double sum = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    sum += numbers[i];
+}
+Console.WriteLine($"Suma elementów: {sum}");
+
+double product = 1;
+for (int i = 0; i < numbers.Length; i++)
+{
+    product *= numbers[i];
+}
+Console.WriteLine($"Iloczyn elementów: {product}");
+
+double average = sum / numbers.Length;
+Console.WriteLine($"Średnia elementów: {average}");
+
+double min = numbers[0];
+for (int i = 1; i < numbers.Length; i++)
+{
+    if (numbers[i] < min)
+    {
+        min = numbers[i];
+    }
+}
+Console.WriteLine($"Wartość minimalna: {min}");
+
+double max = numbers[0];
+for (int i = 1; i < numbers.Length; i++)
+{
+    if (numbers[i] > max)
+    {
+        max = numbers[i];
+    }
+}
+Console.WriteLine($"Wartość maksymalna: {max}");
