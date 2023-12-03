fc63489ca60c931897b63f2ebda2878e3b7e69cf
mr-gino <skocz.dawid@o2.pl>
2023-12-03 22:13:10 +0100

    dodanie zadania 1 lab 1

---
+++ lab1/lab1.1/Program.cs
@@ -0,0 +1,10 @@
+﻿double a = 1;
+double b = 3;
+double c = -4;
+
+double delta = Math.Pow(b, 2) - 4 * a * c;
+
+double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+
+Console.WriteLine($"Delta wynosi: {delta}, x1 = {x1}, x2 = {x2}");
