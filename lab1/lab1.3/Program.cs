0cc6e1a5841e638bb88f3ec476c1846c3013d2e5
mr-gino <skocz.dawid@o2.pl>
2023-12-03 22:32:07 +0100

    dodanie zadadania 3 lab 1

---
+++ lab1/lab1.3/Program.cs
@@ -0,0 +1,39 @@
+﻿double[] numbers = new double[10];
+
+Console.WriteLine("Wprowadź 10 liczb rzeczywistych:");
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write($"Liczba {i + 1}: ");
+    numbers[i] = Convert.ToDouble(Console.ReadLine());
+}
+
+Console.WriteLine("\nWyświetlanie tablicy od pierwszego do ostatniego indeksu:");
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write(numbers[i] + " ");
+}
+
+Console.WriteLine("\n\nWyświetlanie tablicy od ostatniego do pierwszego indeksu:");
+
+for (int i = numbers.Length - 1; i >= 0; i--)
+{
+    Console.Write(numbers[i] + " ");
+}
+
+Console.WriteLine("\n\nWyświetlanie elementów o nieparzystych indeksach:");
+
+for (int i = 1; i < numbers.Length; i += 2)
+{
+    Console.Write(numbers[i] + " ");
+}
+
+Console.WriteLine("\n\nWyświetlanie elementów o parzystych indeksach:");
+
+for (int i = 0; i < numbers.Length; i += 2)
+{
+    Console.Write(numbers[i] + " ");
+}
+
+Console.ReadLine();
