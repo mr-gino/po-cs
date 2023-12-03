26a2ed800c84db7c343f5e7b49aa2f9a5b9baf96
mr-gino <skocz.dawid@o2.pl>
2023-12-03 22:55:53 +0100

    dodanie zadania 3 lab 2

---
+++ lab2/lab2.3/Program.cs
@@ -0,0 +1,42 @@
+﻿class MojaData
+{
+    private DateTime data;
+
+    public MojaData()
+    {
+        data = DateTime.Now;
+    }
+
+    public DateTime PobierzBieżącąDatę()
+    {
+        return data;
+    }
+
+    public void PrzestawNaTydzieńWPrzód()
+    {
+        data = data.AddDays(7);
+    }
+
+    public void PrzestawNaTydzieńWTył()
+    {
+        data = data.AddDays(-7);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        MojaData mojaData = new MojaData();
+
+        Console.WriteLine("Bieżąca data: " + mojaData.PobierzBieżącąDatę());
+
+        mojaData.PrzestawNaTydzieńWPrzód();
+        Console.WriteLine("Data po przestawieniu o tydzień w przód: " + mojaData.PobierzBieżącąDatę());
+
+        mojaData.PrzestawNaTydzieńWTył();
+        Console.WriteLine("Data po przestawieniu o tydzień w tył: " + mojaData.PobierzBieżącąDatę());
+
+        Console.ReadLine();
+    }
+}
