6e039da596ea7e4e58bde974d185bf5174e5e3a2
mr-gino <skocz.dawid@o2.pl>
2023-12-03 22:51:58 +0100

    dodanie zadania 1 lab 2

---
+++ lab2/lab2.1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Licz
+{
+    private int value;
+
+    public Licz(int initialValue)
+    {
+        value = initialValue;
+    }
+
+    public void Dodaj(int number)
+    {
+        value += number;
+    }
+
+    public void Odejmij(int number)
+    {
+        value -= number;
+    }
+
+    public void WypiszStan()
+    {
+        Console.WriteLine($"Wartość: {value}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Licz licz1 = new Licz(10);
+        Licz licz2 = new Licz(5);
+
+        licz1.Dodaj(7);
+        licz2.Odejmij(3);
+
+        licz1.WypiszStan();
+        licz2.WypiszStan();
+    }
+}
